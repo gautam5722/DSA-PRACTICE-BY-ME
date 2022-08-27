2ccb257f7ffcc45acfc88d3668a9e5b1f153c48a
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2022-08-28 01:47:00 +0530

    Update Solution.cpp

--- GFG/Segregate 0s and 1s/Solution.cpp
+++ GFG/Segregate 0s and 1s/Solution.cpp
@@ -60,3 +60,19 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+
+
+
+// class Solution{   
+// public:
+//     void segregate0and1(int arr[], int n) {
+//         int start = 0 ;
+//         for(int i=0; i < n; i++)
+//         {
+//             if(arr[i] == 0)
+//             {
+//                 swap(arr[i] , arr[start++]);
+//             }
+//         }
+//     }
+// };
