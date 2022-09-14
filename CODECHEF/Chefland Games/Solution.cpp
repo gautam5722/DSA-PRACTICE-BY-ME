5e540ecc89c66aa8a744a79d63883dbaaa543195
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2022-09-15 01:17:10 +0530

    15 - sep - 2022

---
+++ CODECHEF/Chefland Games/Solution.cpp
@@ -0,0 +1,24 @@
+/*
+Question :- https://www.codechef.com/submit/CHEFGAMES
+Solution :- 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int R1,R2,R3,R4;
+	    cin >> R1 >> R2 >> R3 >> R4;
+	    if(R1 == 0 && R2 == 0 && R3 == 0 && R4 == 0)
+	    {
+	         cout << "IN" << endl;
+	    }
+	    else
+	        cout << "OUT" << endl;
+	}
+	return 0;
+}
