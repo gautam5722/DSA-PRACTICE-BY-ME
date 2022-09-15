9757492f914f03ff58eb91206b41015da5f07a1d
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2022-09-15 11:51:07 +0530

    15 - sep - 2022

---
+++ CODECHEF/Burgers/Solution.cpp
@@ -0,0 +1,27 @@
+/*
+Question :- https://www.codechef.com/submit/BURGERS
+Solution :- 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int a , b; 
+	    cin >> a >> b;
+	   
+	    if(a <= b)
+	    {
+	        cout << a << endl;
+	    }
+	    else if(a>=b)
+	    {
+	        cout << b << endl;
+	    }
+	}
+	return 0;
+}
