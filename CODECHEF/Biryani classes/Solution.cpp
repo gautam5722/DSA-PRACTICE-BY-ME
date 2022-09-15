76dcdb9e9e39a47f9dfb37768a36f63004793a79
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2022-09-15 11:33:34 +0530

    15 - SEP - 2022

---
+++ CODECHEF/Biryani classes/Solution.cpp
@@ -0,0 +1,19 @@
+/*
+Question :- https://www.codechef.com/submit/BIRYANI
+
+Solution :- 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int x , y;
+	    cin >> x >> y;
+	    cout << x * y << endl;
+	}
+	return 0;
+}
